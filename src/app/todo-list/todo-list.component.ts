70206fc57cb02a8e440bca62b0b1af009a5cf975
Tanuka <tanukamondal01@gmail.com>
2024-12-03 22:50:00 +0530

    final changes

--- src/app/todo-list/todo-list.component.ts
+++ src/app/todo-list/todo-list.component.ts
@@ -1,15 +1,41 @@
 import { Component } from '@angular/core';
 import { Todo } from '../models/todo';
+import { OnInit } from '@angular/core';
+
 @Component({
   selector: 'app-todo-list',
   templateUrl: './todo-list.component.html',
   styleUrls: ['./todo-list.component.css']
 })
-export class TodoListComponent {
+export class TodoListComponent implements OnInit {
+  
+  todoName : string = "";
+  todoDate : Date = new Date();
 
-  todo: Todo = {
-    id: 1,
-    title: 'Buy milk',
-    date: new Date('2024-12-02')
+  todos: Todo[] = []
+
+  ngOnInit(): void {
+    let savedTodos = localStorage.getItem("todos")
+    this.todos = savedTodos ? JSON.parse(savedTodos) : []
+  }
+  addTodo(){
+    if(this.todoName.trim().length  && this.todoDate){
+      let newTodo: Todo = {
+        id: Date.now(),
+        title: this.todoName,
+        date: this.todoDate
+      }
+
+      this.todos.push(newTodo);
+
+      this.todoName = "";
+      this.todoDate = new Date();
+      localStorage.setItem("todos", JSON.stringify(this.todos));
+  }
+}
+
+  deleteTodo(index: number){
+    this.todos.splice(index,1)
+    localStorage.setItem("todos", JSON.stringify(this.todos));
   }
 }
